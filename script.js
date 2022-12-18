06035c9ce5d4c7f7f4bc17040f5604096ee443cf
Stian Marton <stianmarton2@gmail.com>
2022-12-18 15:21:01 +0100

    Updated CSS, "Eraser" added, "Change Grid" fixed

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 let grid = document.querySelector(".gridContainer");
 const btnReset = document.getElementById('reset');
 const btnRainbow = document.getElementById('rainbow');
+const btnEraser = document.getElementById('eraser');
 let number = 16;
 let buttonClicked = false;
 let randomColor = "";
-
+let eraserEnabled = false;
 
 reset.addEventListener("click", clearGrid)
+
+eraser.addEventListener("click", function() {
+    eraserEnabled = !eraserEnabled;
+    if (eraserEnabled) {
+        btnEraser.innerHTML = "Eraser ON";
+    } else if (!eraserEnabled) {
+        btnEraser.innerHTML = "Eraser OFF";
+    }
+})
 
 rainbow.addEventListener("click", function() {
     buttonClicked = !buttonClicked;
@@ -22,30 +32,34 @@ function createGrid() {
     for (let i = 0; i < number*number; i++) {
         let div = document.createElement("div");
         grid.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
-        grid.style.gridTemplateTows = `repeat(${number}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${number}, 1fr)`;
         div.classList.add("square");
         grid.appendChild(div); 
         div.addEventListener('mouseover', mouseOver) 
             function mouseOver() {
-                if (buttonClicked) {
-                    randomColor =
-                    Math.floor(Math.random()*16777215).toString(16);
+                if (buttonClicked && !eraserEnabled) {
+                    randomColor = Math.floor(Math.random()*16777215).toString(16);
                     div.style.backgroundColor = "#" + randomColor;
-                } else if (!buttonClicked) {
+                    div.style.transition = "0.2s";
+                } else if (!buttonClicked && !eraserEnabled) {
                     div.style.backgroundColor = "black";
+                    div.style.transition = "0.2s";
+                } else if (eraserEnabled) {
+                    div.style.backgroundColor = "#d9d9d9";
+                    div.style.transition = "0s";
                 }
             } 
     }
 }
 
 function clearGrid() {
-    let arrayGrid = [...document.querySelectorAll("div")]
+    let arrayGrid = [...document.querySelectorAll(".square")]
     arrayGrid.forEach(div=>div.style.backgroundColor = "#d9d9d9");
 }
 
 function changeGrid() {
     clearGrid();
-    number = prompt("Please enter a number from 1 to 100");
+    number = prompt("Please enter a number from 2 to 100 (16 default)");
     if (number > 1 && number <= 100) { 
         createGrid(number);
     } else {
